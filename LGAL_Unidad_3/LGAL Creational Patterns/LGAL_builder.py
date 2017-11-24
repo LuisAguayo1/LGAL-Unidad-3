9b3d7d092aeaf53b61b33d55b857d678c0f2b057
LuisAguayo1 <luistxraguayo@gmail.com>
2017-11-24 00:23:30 -0600

    Add files via upload

---
+++ LGAL_Unidad_3/LGAL Creational Patterns/LGAL_builder.py
@@ -0,0 +1,54 @@
+"""Luis Gerardo Aguayo López
+    GITI9072
+    Creational patterns"""
+class Director():
+    """Director"""
+    def __init__(self, builder):
+        self._builder = builder
+
+    def construct_car(self):
+        self.builder.create_new_car()
+        self.builder.add_model()
+        self.builder.add_tires()
+        self.builder.add_engine()
+
+    def get_car(self):
+        return self._builder.car
+
+
+class Builder():
+    """Abstract Builder"""
+    def __init__(self):
+        self.car = None
+
+    def create_new_car(self):
+        self.car = Car()
+
+
+class SkyLarkBuilder(Builder):
+    """Concrete Builder --> provides parts and tools to work on the parts"""
+
+    def add_model(self):
+        self.car.model = "Skylark"
+
+    def add_tires(self):
+        self.car.tires = "Regular tires"
+
+    def add_engine(self):
+        self.car.engine = "Turbo engine"
+
+class Car():
+    """Product"""
+    def __init__(self):
+        self.model = None
+        self.tires = None
+        self.engine = None
+
+    def __str__(self):
+        return '{} | {} | {}'.format(self.model, self.tires, self.engine)
+
+builder = SkyLarkBuilder()
+director = Director(builder)
+director.construct_car()
+car = director.get_car()
+print(car)
---
+++ LGAL_Unidad_3/LGAL behavioral patterns/iterator.py
@@ -0,0 +1,18 @@
+"""Luis Gerardo Aguayo López
+    GITI9072
+    behavioral patterns"""
+def count_to(count):
+
+    numbers_in_german = ["eins", "zwei", "drei", "vier", "funf"]
+
+    iterator = zip(range(count), numbers_in_german)
+
+    for position, number in iterator:
+
+        yield number
+
+for num in count_to(3):
+    print("{}".format(num))
+
+for num in count_to(4):
+    print("{}".format(num))
